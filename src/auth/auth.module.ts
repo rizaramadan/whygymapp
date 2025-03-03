806d2a5a1e80a0440976965b3a939385803f713f
Riza Ramadan <riza.ramadan@gmail.com>
2025-03-04 06:31:15 +0700

    feat: auth complete, next is role

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from '../users/users.module';
+import { AuthController } from './auth.controller';
+import { AuthGuard } from './auth.guard';
+import { AuthService } from './auth.service';
+import { jwtConstants } from './constants';
 
 @Module({
-  imports: [UsersModule],
-  providers: [AuthService],
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  providers: [
+    AuthService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
   controllers: [AuthController],
+  exports: [AuthService],
 })
 export class AuthModule {}
