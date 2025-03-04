ed28a7bd4b349b2e1bdbb8967e8f454c2419e333
Riza Ramadan <riza.ramadan@gmail.com>
2025-03-04 07:54:52 +0700

    feat: add env

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,20 +1,21 @@
 import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthGuard } from './auth.guard';
 import { AuthService } from './auth.service';
-import { jwtConstants } from './constants';
 import { RolesGuard } from 'src/roles/roles.guard';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     UsersModule,
     JwtModule.register({
       global: true,
-      secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: process.env.JWT_EXPIRATION || '60s' },
     }),
   ],
   providers: [
